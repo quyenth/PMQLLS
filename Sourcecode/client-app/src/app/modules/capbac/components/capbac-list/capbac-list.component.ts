589d5bf91d91c2cd7aecd29c6b94038cb86311ac
DungNQ10 <dungftuk48@gmail.com>
2018-11-28 19:17:56 +0700

    commit

--- Sourcecode/client-app/src/app/modules/capbac/components/capbac-list/capbac-list.component.ts
+++ Sourcecode/client-app/src/app/modules/capbac/components/capbac-list/capbac-list.component.ts
@@ -14,6 +14,7 @@ import { Subscription } from 'rxjs';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ConfirmationDialogService } from 'src/app/shared/services/confirmDialog.service';
 import { OrderInfo } from 'src/app/shared/models/order-info';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-capbac-list',
